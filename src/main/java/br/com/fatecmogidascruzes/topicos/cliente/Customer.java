a0ef3f24968a36c4021d97ad8a23179e98a4ed61
Leandro Luque <leandro.luque@gmail.com>
2023-03-08 21:46:04 -0300

    feat: create a customer CRUD

---
+++ src/main/java/br/com/fatecmogidascruzes/topicos/cliente/Customer.java
@@ -0,0 +1,61 @@
+package br.com.fatecmogidascruzes.topicos.cliente;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String itin;
+    private String name;
+    private String phone;
+    private String email;
+
+    public String getItin() {
+        return itin;
+    }
+
+    public void setItin(String itin) {
+        this.itin = itin;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
