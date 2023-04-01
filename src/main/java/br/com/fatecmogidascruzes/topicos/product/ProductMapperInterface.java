71c73e1774d3c140ba7a44851007749024d54cb6
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-04-01 10:53:38 -0300

    add Interface for ProductMappers

---
+++ src/main/java/br/com/fatecmogidascruzes/topicos/product/ProductMapperInterface.java
@@ -0,0 +1,6 @@
+package br.com.fatecmogidascruzes.topicos.product;
+
+import br.com.fatecmogidascruzes.topicos.utils.mapper.ObjectMapper;
+
+public interface ProductMapperInterface extends ObjectMapper<Product,ProductDTO> {
+}
