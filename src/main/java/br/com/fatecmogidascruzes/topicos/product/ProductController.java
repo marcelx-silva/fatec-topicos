cdb676c2e3cc05be22df74f01ddb113605577aaa
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-04-01 16:34:15 -0300

    add ServiceLocator ProductMapper

--- src/main/java/br/com/fatecmogidascruzes/topicos/product/ProductController.java
+++ src/main/java/br/com/fatecmogidascruzes/topicos/product/ProductController.java
@@ -1,11 +1,11 @@
 package br.com.fatecmogidascruzes.topicos.product;
 
 import br.com.fatecmogidascruzes.topicos.product.useCase.*;
+import br.com.fatecmogidascruzes.topicos.utils.mapper.ServiceLocator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/products")
@@ -13,10 +13,16 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+    private final ProductMapperInterface productMapper;
+
+    public ProductController() {
+        this.productMapper = ServiceLocator.productMapper();
+    }
 
 
     @PostMapping
-    public void create(@RequestBody Product product){
+    public void create(@RequestBody ProductDTO productDTO){
+        Product product = productMapper.toObject(productDTO);
         new CreateProduct(productRepository).execute(product);
     }
 
@@ -41,7 +47,8 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
-    public void update(@PathVariable Long id, @RequestBody Product product){
+    public void update(@PathVariable Long id, @RequestBody ProductDTO productDTO){
+        Product product = productMapper.toObject(productDTO);
         new UpdateProduct(productRepository).execute(product,id);
 
     }
